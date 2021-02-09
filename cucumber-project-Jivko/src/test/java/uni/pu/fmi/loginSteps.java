4700f9237f33068b7180deec930ae629cc06a313
Jivko <jivko.ivanov14@gmail.com>
2021-02-09 19:53:47 +0200

    Init project

---
+++ cucumber-project-Jivko/src/test/java/uni/pu/fmi/loginSteps.java
@@ -0,0 +1,40 @@
+package uni.pu.fmi;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class loginSteps {
+	@Given("^Потребитеял отваря екрана за вход в системата$")
+	public void потребитеял_отваря_екрана_за_вход_в_системата() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Потребителя въведе потребителско име \"([^\"]*)\"$")
+	public void потребителя_въведе_потребителско_име(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Въведе парола \"([^\"]*)\"$")
+	public void въведе_парола(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Натисне върху бутона за вход в системата$")
+	public void натисне_върху_бутона_за_вход_в_системата() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Вижда съобщение \"([^\"]*)\"$")
+	public void вижда_съобщение(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+}
